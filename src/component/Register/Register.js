4c3fb4093ee1765873f8ffd3e7c7299cbdd3a7a9
Ethan <phaladone@gmail.com>
2019-07-08 15:46:23 -0700

    heroku attempt

--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends Component {
         body: JSON.stringify(this.state),
         headers:{
             "Content-type" : 'application/json',
-            'Access-Control-Allow-Origin':  'https://secure-savannah-81040.herokuapp.com',
+            'Access-Control-Allow-Origin':  window.location.origin,
             'Access-Control-Allow-Methods': 'POST',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization'
             
